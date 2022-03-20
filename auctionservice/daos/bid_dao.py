b4eaf7925d27496ed62031073b5505283e4dd02a
max1999mae <maxkanters@hotmail.nl>
2022-03-20 17:01:47 +0100

    auction

--- auctionservice/daos/bid_dao.py
+++ auctionservice/daos/bid_dao.py
@@ -12,7 +12,7 @@ class BidDAO(Base):
     bid_amount = Column(Float)
 
     auction_id = Column(Integer, ForeignKey('auction.id'))
-    auction = relationship(AuctionIdDAO.__name__, backref=backref("auction"), uselist=False))
+    auction = relationship(AuctionIdDAO.__name__, backref=backref("auction", uselist=False))
 
     status_id = Column(Integer, ForeignKey('status.id'))
     status = relationship(BidStatusDAO.__name__, backref=backref("bid", uselist=False))
@@ -22,6 +22,3 @@ class BidDAO(Base):
         self.bid_amount = bid_amount
         self.auction = auction
         self.status = status
-
-
-
