3031406642b5fe453d57a4793cd938fd612414d4
koen430 <34771783+koen430@users.noreply.github.com>
2022-03-22 14:15:07 +0100

    Update main.py

--- Assignment 1/searchservice/main.py
+++ Assignment 1/searchservice/main.py
@@ -3,13 +3,13 @@ from resources.db_util import DBUtil
 
 def store_history(request):
 	request_json = request.get_json(silent=True)
-    request_args = request.args.get()
+	request_args = request.args
 	if request and 'arg1' in request_json:
-        arg1 = request_json['arg1']
-    elif request_args and 'arg1' in request_args:
-        arg1 = request_args['arg1']
-    else:
-        continue
+		arg1 = request_json['arg1']
+	elif request_args and 'arg1' in request_args:
+		arg1 = request_args['arg1']
+	else:
+		continue
 	db_util = DBUtil()
-	search = request.args
+	search = arg1
 	db_util.add_data_records('history-db', search)
