0f57faf11f40846c7ef9e11fc0e6741aa0dbff7d
koen430 <34771783+koen430@users.noreply.github.com>
2022-03-23 12:12:28 +0100

    Update main.py

--- Assignment 1/searchservice_v2/history_faas/main.py
+++ Assignment 1/searchservice_v2/history_faas/main.py
@@ -8,6 +8,8 @@ def update_history(request):
     table_id = "ada-search-service.searchhistory_db.search_history"
 
     request_json = request.get_json(silent=True)
+    args = request.args
+
 
     if 'search' in request_json:
         new_search = request_json['search']
@@ -19,6 +21,15 @@ def update_history(request):
             return abort(405)
         else:
             return json.dumps({'message': 'a search was added'}, sort_keys=False, indent=4), 200
+    if 'search' in args:
+        new_search = args['search']
 
+        rows_to_insert = [{'search':new_search}]
+        errors = client.insert_rows_json(table_id, rows_to_insert, row_ids=[None] * len(rows_to_insert))
+
+        if errors != []:
+            return abort(405)
+        else:
+            return json.dumps({'message': 'a search was added'}, sort_keys=False, indent=4), 200
     else:
         return json.dumps({'message': 'a search could not be added'}, sort_keys=False, indent=4), 200
