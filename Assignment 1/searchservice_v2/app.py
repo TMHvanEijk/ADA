8842f7c8ed81781d3cce03d7ea3b649c84b7ad33
koen430 <34771783+koen430@users.noreply.github.com>
2022-03-23 16:16:37 +0100

    new_working version

--- Assignment 1/searchservice_v2/app.py
+++ Assignment 1/searchservice_v2/app.py
@@ -2,33 +2,8 @@ from flask import Flask, json , request, Response
 import requests
 from google.cloud import bigquery
 
-from resources.db_util import DBUtil
-
 app = Flask(__name__)
 app.config["DEBUG"] = True
-db_util = DBUtil()
-
-
-@app.route('/searchservice/<table_name>', methods=['POST'])
-def create_table(table_name):
-    # get the payload or body
-    req_data = request.get_json()
-    columns = req_data['columns']
-    db_util.create_tb(table_name, columns)
-    return json.dumps({'message': 'a table was created'}, sort_keys=False, indent=4), 200
-
-
-@app.route('/searchservice/<table_name>', methods=['DELETE'])
-def delete_table(table_name):
-    db_util.drop_tb(table_name)
-    return json.dumps({'message': 'a table was dropped'}, sort_keys=False, indent=4), 200
-
-
-@app.route('/searchservice/<table_name>', methods=['PUT'])
-def update_data(table_name):
-    content = request.get_json()
-    db_util.add_data_records(table_name, content)
-    return json.dumps({'message': '{} data were updated'.format(table_name)}, sort_keys=False, indent=4), 200
 
 
 @app.route('/searchservice/<search>', methods=['GET'])
