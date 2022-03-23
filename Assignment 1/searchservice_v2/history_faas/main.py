d0879634142dfd8d88996c219f41b87752064a27
koen430 <34771783+koen430@users.noreply.github.com>
2022-03-23 11:15:26 +0100

    Update main.py

--- Assignment 1/searchservice_v2/history_faas/main.py
+++ Assignment 1/searchservice_v2/history_faas/main.py
@@ -1,16 +1,20 @@
 
 def update_history(request):
     from flask import abort
-    from google import bigquery
+    from google.cloud import bigquery
 
     table_id = "ada-search-service.searchhistory_db.search_history"
-    new_search = request['search']
 
-    row_to_insert = {'search':search}
+    if request['search']:
+        new_search = request['search']
 
-    errors = client.insert_rows_json(table_id, rows_to_insert, row_ids=[None] * len(rows_to_insert))
+        row_to_insert = {'search':search}
+        errors = client.insert_rows_json(table_id, rows_to_insert, row_ids=[None] * len(rows_to_insert))
 
-    if errors != []:
-        return abort(405)
+        if errors != []:
+            return abort(405)
+        else:
+            return json.dumps({'message': 'a search was added'}, sort_keys=False, indent=4), 200
+
     else:
-        return json.dumps({'message': 'a search was added'}, sort_keys=False, indent=4), 200
+        return json.dumps({'message': 'a search could not be added'}, sort_keys=False, indent=4), 400
